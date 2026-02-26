8a7e483b69e065970a9c7521f984a74a0820f261
Mat. Hdez. <mmateo95@protonmail.com>
2026-02-25 19:04:01 -0600

    Fix indentation and formatting in proyectar lambda

--- graficador.cpp
+++ graficador.cpp
@@ -226,13 +226,12 @@ void MyGraphCanvas::Dibujar3D(wxGraphicsContext* gc, int w, int h) {
         // Intercambiamos X e Y aquí mismo
         double x = yOriginal;
         double y = xOriginal;
-
         double x1 = x * cos(angleYaw) - y * sin(angleYaw);
         double y1 = x * sin(angleYaw) + y * cos(angleYaw);
         double yp = y1 * cos(anglePitch) - z * sin(anglePitch);
         px = centroX + (x1 * escala);
         py = centroY + (yp * escala);
-    };
+    }
 
     wxFont fuenteTextos = wxSystemSettings::GetFont(wxSYS_DEFAULT_GUI_FONT);
     fuenteTextos.SetPointSize(9);
